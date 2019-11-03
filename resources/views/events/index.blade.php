a32f97928a7ce9a6386b047f72da9fbe4327d38c
Julien Bourdeau <julien@sigerr.org>
2019-11-03 19:23:02 +0100

    Fix date time (minutes)

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -21,7 +21,7 @@
                 <div class="bg-gray-100 rounded-lg border p-2 text-center w-12 md:w-24 mr-4">
                     <span class="hidden md:block text-sm">{{ $event->start_time->format('D') }}</span>
                     <span class="md:block font-bold">{{ $event->start_time->format('d M') }}</span>
-                    <span class="hidden md:block">{{ $event->start_time->format('H:m') }}</span>
+                    <span class="hidden md:block">{{ $event->start_time->format('H:i') }}</span>
                 </div>
             </div>
             <div>
