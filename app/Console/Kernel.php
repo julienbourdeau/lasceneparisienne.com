937162f441290e2c6d56e1907743432da168d02d
Julien Bourdeau <julien@sigerr.org>
2019-11-09 09:19:03 +0100

    Run import:facebook twice daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,10 +24,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        if ($this->app->environment('production')) {
-            $schedule->command('backup:clean')->daily()->at('01:00');
-            $schedule->command('backup:run')->daily()->at('02:00');
-        }
+        $schedule->command('import:facebook')->twiceDaily(8, 16);
+
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('02:00');
     }
 
     /**
