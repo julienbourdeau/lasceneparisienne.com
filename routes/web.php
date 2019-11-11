ecb7035c8dcc84f6b2f24dd257082e19ac3823d9
Julien Bourdeau <julien@sigerr.org>
2019-11-11 18:40:41 +0100

    URL en francais

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,21 @@
 //Route::get('/', 'HomeController')->name('home');
 Route::redirect('/', '/events')->name('home');
 
-Route::get('/events', 'EventListController@index')->name('events');
-Route::get('/event/{slug}', 'EventController@show')->name('event');
+Route::get('/concerts', 'EventListController@index')->name('events');
+Route::get('/concert/{slug}', 'EventController@show')->name('event');
 
-Route::get('/venues', 'VenueListController@index')->name('venues');
-Route::get('/venue/{slug}', 'VenueController@show')->name('venue');
+Route::get('/salles-concerts', 'VenueListController@index')->name('venues');
+Route::get('/salle-concert/{slug}', 'VenueController@show')->name('venue');
 
 
 Route::middleware([\App\Http\Middleware\AuthorizeFacebookConnect::class])->group(function () {
     Route::get('fb/login', 'FacebookConnectController@login')->name('fb.login');
     Route::get('fb/callback', 'FacebookConnectController@callback')->name('fb.callback');
 });
+
+
+// Redirection retrocompat'
+Route::permanentRedirect('/events', '/concerts');
+Route::permanentRedirect('/event/{slug}', '/concert/{slug}');
+Route::permanentRedirect('/venues', '/salles-concert');
+Route::permanentRedirect('/venue/{slug}', '/salle-concert/{slug}');
