9500279f3b664dbf7a7f0d65ee8c3c3d0071cf07
Julien Bourdeau <julien@sigerr.org>
2019-11-17 16:00:38 +0100

    Update cover for events in 30 days

--- app/Console/Commands/ImportFacebookEventCommand.php
+++ app/Console/Commands/ImportFacebookEventCommand.php
@@ -116,7 +116,7 @@ class ImportFacebookEventCommand extends Command
             return false;
         }
 
-        if ($event->start_time->timestamp < now()->addDays(14)->timestamp) {
+        if ($event->start_time->timestamp < now()->addDays(30)->timestamp) {
             return true;
         }
 
