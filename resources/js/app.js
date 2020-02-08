b991684d14be93d34a52cecc93612c9c1e1191c8
Julien Bourdeau <julien@sigerr.org>
2020-02-08 10:13:50 +0100

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,21 @@
 require('./bootstrap');
 
+import Vue from 'vue';
+import InstantSearch from 'vue-instantsearch';
+import algoliasearch from 'algoliasearch/lite';
+
+Vue.use(InstantSearch);
+
+new Vue({
+    el: '#app',
+    data: {
+        searchClient: algoliasearch(
+            'YourApplicationID',
+            'YourAdminAPIKey',
+        ),
+    },
+});
+
 window.toggleMenu = function() {
     $('#nav').slideToggle();
 }
