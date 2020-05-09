1057d3aa47fbb6cb3ed092af2f9d533a8276f807
Julien Bourdeau <julien@sigerr.org>
2020-05-09 11:13:22 +0200

    Show api response on event page (for admins)

--- resources/views/_partials/admin-actions.blade.php
+++ resources/views/_partials/admin-actions.blade.php
@@ -1,5 +1,8 @@
 @auth
     <span class="font-medium uppercase text-sm">
+        @if(isset($apiResponse) && $apiResponse)
+            <span @click="apiResponse = !apiResponse" x-text="!apiResponse ? 'Show json' : 'Hide json'" class="text-green-600 pr-2">Show json</span>
+        @endif
         <a href="{{ adminView($model) }}" class="text-blue-600 pr-2">View</a>
         <a href="{{ adminEdit($model) }}" class="text-red-700">Edit</a>
     </span>
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -17,62 +17,66 @@
 
     @include('_partials.breadcrumb')
 
-    <div class="lg:flex lg:flex-row-reverse">
-        <div class="mt-12 mb-12 lg:mb-2 lg:w-1/3 lg:pl-8 lg:mt-0">
-            <img class="w-full rounded-bl-lg object-center object-cover" src="{{ $event->cover }}" alt="{{ $event->name }}">
+    <div x-data="{apiResponse: false}">
 
-            <div class="hidden lg:block">
-                <a class="block pt-4 hover:text-red-800" href="{{ route('event.ics', $event->uuid) }}">Ajoutez à votre agenda</a>
-                <a class="block pt-4 hover:text-blue-600" href="https://www.facebook.com/events/{{ $event->id_facebook }}/">
-                    Voir sur facebook
-                </a>
+        <div class="lg:flex lg:flex-row-reverse">
+            <div class="mt-12 mb-12 lg:mb-2 lg:w-1/3 lg:pl-8 lg:mt-0">
+                <img class="w-full rounded-bl-lg object-center object-cover" src="{{ $event->cover }}" alt="{{ $event->name }}">
+
+                <div class="hidden lg:block">
+                    <a class="block pt-4 hover:text-red-800" href="{{ route('event.ics', $event->uuid) }}">Ajoutez à votre agenda</a>
+                    <a class="block pt-4 hover:text-blue-600" href="https://www.facebook.com/events/{{ $event->id_facebook }}/">
+                        Voir sur facebook
+                    </a>
+                </div>
+            </div>
+
+            <div class="lg:w-2/3">
+
+                <h1 class="text-xl md:text-2xl lg:text-3xl font-bold mb-8 leading-none">
+                    {{ $event->name }}
+                </h1>
+
+                @include('_partials.admin-actions', ['model' => $event, 'apiResponse' => true])
+
+                @if($event->canceled)
+                    <span class="text-xs uppercase border rounded py-2 px-3 font-bold bg-red-900 text-white tracking-wide">Annulé</span>
+                @elseif($event->soldout)
+                    <span class="text-xs uppercase border rounded py-2 px-3 font-bold bg-gray-700 text-white tracking-wide">Complet</span>
+                @endif
+
+                <div class="my-6 md:my-12 p-4 bg-yellow-100">
+                    <p class="font-semibold text-red-900">{{ $event->start_time->format('l d F Y') }}</p>
+                    <p class="text-gray-800">{{ $event->start_time->format('H:i') }} - {{ $event->end_time->format('H:i') }}</p>
+                    <br>
+                    <a href="{{ $event->venue->canonical_url }}">
+                        <h6 class="font-semibold text-red-900">{{ $event->venue->name }}</h6>
+                        <p class="text-gray-800">{{ $event->venue->address_formatted }}</p>
+                    </a>
+                </div>
+
+                <div class="fb-event-description mt-8 text-sm leading-tight md:text-base">
+                    {!! $event->description_html !!}
+                </div>
+
             </div>
         </div>
 
-        <div class="lg:w-2/3">
-
-            <h1 class="text-xl md:text-2xl lg:text-3xl font-bold mb-8 leading-none">
-                {{ $event->name }}
-            </h1>
-
-            @include('_partials.admin-actions', ['model' => $event])
-
-            @if($event->canceled)
-                <span class="text-xs uppercase border rounded py-2 px-3 font-bold bg-red-900 text-white tracking-wide">Annulé</span>
-            @elseif($event->soldout)
-                <span class="text-xs uppercase border rounded py-2 px-3 font-bold bg-gray-700 text-white tracking-wide">Complet</span>
-            @endif
-
-            <div class="my-6 md:my-12 p-4 bg-yellow-100">
-                <p class="font-semibold text-red-900">{{ $event->start_time->format('l d F Y') }}</p>
-                <p class="text-gray-800">{{ $event->start_time->format('H:i') }} - {{ $event->end_time->format('H:i') }}</p>
-                <br>
-                <a href="{{ $event->venue->canonical_url }}">
-                    <h6 class="font-semibold text-red-900">{{ $event->venue->name }}</h6>
-                    <p class="text-gray-800">{{ $event->venue->address_formatted }}</p>
-                </a>
-            </div>
-
-            <div class="fb-event-description mt-8 text-sm leading-tight md:text-base">
-                {!! $event->description_html !!}
-            </div>
-
+        <div class="flex justify-around mt-12 text-sm text-center lg:hidden">
+            <a class="block px-3 mx-2 py-1 bg-gray-200 rounded hover:text-red-800" href="{{ route('event.ics', $event->uuid) }}">Ajoutez à votre agenda</a>
+            <a class="block px-3 mx-2 py-1 bg-gray-200 rounded hover:text-blue-600" href="{{ $event->facebook_url }}">
+                Voir sur facebook
+            </a>
         </div>
-    </div>
 
-    <div class="flex justify-around mt-12 text-sm text-center lg:hidden">
-        <a class="block px-3 mx-2 py-1 bg-gray-200 rounded hover:text-red-800" href="{{ route('event.ics', $event->uuid) }}">Ajoutez à votre agenda</a>
-        <a class="block px-3 mx-2 py-1 bg-gray-200 rounded hover:text-blue-600" href="{{ $event->facebook_url }}">
-            Voir sur facebook
-        </a>
-    </div>
+        @auth
+            <div x-show="apiResponse" class="my-16 p-4 bg-gray-100 border" style="display: none;">
+                @php
+                  $apiResponse = json_encode($event->toArray(), JSON_PRETTY_PRINT);
+                @endphp
+                {!! markdown("```json\n {$apiResponse} \n```") !!}
+            </div>
+        @endauth
 
-    @auth
-        <ul class="m-16 bg-gray-100 border py-4 px-8">
-            <li>Id: #{{ $event->id }}</li>
-            <li>UUID: {{ $event->uuid }}</li>
-            <li>Created at: {{ $event->created_at }}</li>
-            <li>Updated at: {{ $event->updated_at }}</li>
-        </ul>
-    @endauth
+    </div>
 @endsection
