8fe70e18a094235f965e533cd23bd31e1d467d67
Julien Bourdeau <julien@sigerr.org>
2020-05-13 19:59:50 +0200

    clean up routes

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,34 @@
 <?php
 
-Route::get('/', 'HomeController')->name('home');
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\ArchiveController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\EventListController;
+use App\Http\Controllers\FacebookConnectController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IcalController;
+use App\Http\Controllers\VenueController;
+use App\Http\Controllers\VenueListController;
+
+Route::get('/', HomeController::class)->name('home');
 
 Route::feeds();
-Route::get('cal.ics', 'IcalController@feed')->name('ics');
-Route::get('/{uuid}.ics', 'IcalController@event')->name('event.ics');
-Route::get('/public-api', 'ApiController')->name('page.api');
+Route::get('cal.ics', [IcalController::class, 'feed'])->name('ics');
+Route::get('/{uuid}.ics', [IcalController::class, 'event'])->name('event.ics');
 
-Route::get('/concerts', 'EventListController@index')->name('events');
-Route::get('/concert/{slug}', 'EventController@show')->name('event');
-Route::get('/archives/{period?}', 'ArchiveController@index')->name('archives');
+Route::get('/public-api', ApiController::class)->name('page.api');
 
-Route::get('/salles-concerts', 'VenueListController@index')->name('venues');
-Route::get('/salle-concert/{slug}', 'VenueController@show')->name('venue');
+Route::get('/concerts', [EventListController::class, 'index'])->name('events');
+Route::get('/concert/{slug}', [EventController::class, 'show'])->name('event');
+Route::get('/archives/{period?}', [ArchiveController::class, 'index'])->name('archives');
+
+Route::get('/salles-concerts', [VenueListController::class, 'index'])->name('venues');
+Route::get('/salle-concert/{slug}', [VenueController::class, 'show'])->name('venue');
 
 
 Route::middleware([\App\Http\Middleware\AuthorizeFacebookConnect::class])->group(function () {
-    Route::get('fb/login', 'FacebookConnectController@login')->name('fb.login');
-    Route::get('fb/callback', 'FacebookConnectController@callback')->name('fb.callback');
+    Route::get('fb/login', [FacebookConnectController::class, 'login'])->name('fb.login');
+    Route::get('fb/callback', [FacebookConnectController::class, 'callback'])->name('fb.callback');
 });
 
 
